0e144d6bda0a6fc47049e2b94da80fb407d2ed86
Noel Varghese <91435868+NoelVarghese2006@users.noreply.github.com>
2025-02-15 22:52:39 -0600

    init

---
+++ components/Header.tsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <div className="flex justify-between p-4">
+        <div className="text-2xl">ABA</div>
+        <div className="text-2xl">Contact Us</div>
+    </div>
+  )
+}
+
+export default Header
