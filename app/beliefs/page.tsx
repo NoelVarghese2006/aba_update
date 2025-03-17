acb490be237c3696317241ae1d771f87247f63ab
Noel Varghese <91435868+NoelVarghese2006@users.noreply.github.com>
2025-03-16 22:57:44 -0500

    hovercard verses

--- app/beliefs/page.tsx
+++ app/beliefs/page.tsx
@@ -1,15 +1,158 @@
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card"
+
+
+
+
 
 function Beliefs() {
   return (
     <div className="p-4">
-      <div>That the Bible is the inspired Word of God and is the final authority for all matters of faith, life, and conduct. (2 Timothy 3:16-17, 2 Peter 1:21, John 17:17)</div>
-      <div className="my-4">That the Godhead eternally exists in three persons – the Father, the Son, and the Holy Spirit. God is our Creator, Redeemer, and Sustainer. (Matthew 28:19, 2 Corinthians 13:14, James 1:17)</div>
-      <div className="my-4">That man was created in the image of God and fell in sin by disobedience. (Genesis 2:17, Romans 5:18, Ephesians 2:1-3)</div>
-      <div className="my-4">In the virgin birth of the Lord Jesus Christ and that He is the only Savior. (John 3:16, 14:6, Acts 4:12)</div>
-      <div className="my-4">That salvation is by grace through faith and not of works. Salvation is the gift of God and is eternally secure. (Ephesians 2:8-9, John 10:28, Romans 8:38)</div>
-      <div className="my-4">That the Church is the body of Christ, which began on the day of Pentecost with Jesus Christ as the head. The Church is entrusted with the ministries of worship, edification, and evangelism. (Acts 2:14-47, Ephesians 2:20-23)</div>
-      <div className="my-4">That baptism and the Lord’s Supper are the only ordinances of the Church instituted by Jesus Christ. (Matthew 28:19, 1 Corinthians 11:23-26)</div>
-      <div className="my-4">That the Holy Spirit indwells every believer upon belief in the Lord Jesus.(Acts 2:1-4, Romans 8:9)</div>
+      <div>That the Bible is the inspired Word of God and is the final authority for all matters of faith, life, and conduct.  
+        <HoverCard>
+          <HoverCardTrigger> (2 Timothy 3:16-17, </HoverCardTrigger>
+          <HoverCardContent>
+            'All Scripture is given by inspiration of God, and is profitable for doctrine, for reproof, for correction, for instruction in righteousness, that the man of God may be complete, thoroughly equipped for every good work.' (NKJV)
+          </HoverCardContent>
+        </HoverCard>
+        <HoverCard>
+          <HoverCardTrigger>2 Peter 1:21, </HoverCardTrigger>
+          <HoverCardContent>
+            'for prophecy never came by the will of man, but holy men of God spoke as they were moved by the Holy Spirit.' (NKJV)
+          </HoverCardContent>
+        </HoverCard>
+        <HoverCard>
+          <HoverCardTrigger>John 17:17)</HoverCardTrigger>
+          <HoverCardContent>
+            'Sanctify them by Your truth. Your word is truth.' (NKJV)
+          </HoverCardContent>
+        </HoverCard>
+      </div>
+      <div className="my-4">That the Godhead eternally exists in three persons – the Father, the Son, and the Holy Spirit. God is our Creator, Redeemer, and Sustainer. 
+        <HoverCard>
+          <HoverCardTrigger> (Matthew 28:19, </HoverCardTrigger>
+          <HoverCardContent>
+            'Go therefore and make disciples of all the nations, baptizing them in the name of the Father and of the Son and of the Holy Spirit,' (NKJV)
+          </HoverCardContent>
+        </HoverCard>
+        <HoverCard>
+          <HoverCardTrigger>2 Corinthians 13:14, </HoverCardTrigger>
+          <HoverCardContent>
+            'The grace of the Lord Jesus Christ, and the love of God, and the communion of the Holy Spirit be with you all. Amen.' (NKJV)
+          </HoverCardContent>
+        </HoverCard>
+        <HoverCard>
+          <HoverCardTrigger>James 1:17)</HoverCardTrigger>
+          <HoverCardContent>
+            'Every good gift and every perfect gift is from above, and comes down from the Father of lights, with whom there is no variation or shadow of turning.' (NKJV)
+          </HoverCardContent>
+        </HoverCard>
+      </div>
+      <div className="my-4">That man was created in the image of God and fell in sin by disobedience. 
+        <HoverCard>
+          <HoverCardTrigger> (Genesis 2:17, </HoverCardTrigger>
+          <HoverCardContent>
+            'but of the tree of the knowledge of good and evil you shall not eat, for in the day that you eat of it you shall surely die.”' (NKJV)
+          </HoverCardContent>
+        </HoverCard>
+        <HoverCard>
+          <HoverCardTrigger>Romans 5:18, </HoverCardTrigger>
+          <HoverCardContent>
+            'Therefore, as through one man’s offense judgment came to all men, resulting in condemnation, even so through one Man’s righteous act the free gift came to all men, resulting in justification of life.' (NKJV)
+          </HoverCardContent>
+        </HoverCard>
+        <HoverCard>
+          <HoverCardTrigger>Ephesians 2:1-3)</HoverCardTrigger>
+          <HoverCardContent>
+            'And you He made alive, who were dead in trespasses and sins, in which you once walked according to the course of this world, according to the prince of the power of the air, the spirit who now works in the sons of disobedience, among whom also we all once conducted ourselves in the lusts of our flesh, fulfilling the desires of the flesh and of the mind, and were by nature children of wrath, just as the others.' (NKJV)
+          </HoverCardContent>
+        </HoverCard>
+      </div>
+      <div className="my-4">In the virgin birth of the Lord Jesus Christ and that He is the only Savior. 
+        <HoverCard>
+          <HoverCardTrigger> (John 3:16, </HoverCardTrigger>
+          <HoverCardContent>
+            'For God so loved the world that He gave His only begotten Son, that whoever believes in Him should not perish but have everlasting life.' (NKJV)
+          </HoverCardContent>
+        </HoverCard>
+        <HoverCard>
+          <HoverCardTrigger>John 14:6, </HoverCardTrigger>
+          <HoverCardContent>
+            'Jesus said to him, “I am the way, the truth, and the life. No one comes to the Father except through Me.' (NKJV)
+          </HoverCardContent>
+        </HoverCard>
+        <HoverCard>
+          <HoverCardTrigger>Acts 4:12)</HoverCardTrigger>
+          <HoverCardContent>
+            'Nor is there salvation in any other, for there is no other name under heaven given among men by which we must be saved.”' (NKJV)
+          </HoverCardContent>
+        </HoverCard>
+      </div>
+      <div className="my-4">That salvation is by grace through faith and not of works. Salvation is the gift of God and is eternally secure.  
+        <HoverCard>
+          <HoverCardTrigger> (Ephesians 2:8-9, </HoverCardTrigger>
+          <HoverCardContent>
+            'For by grace you have been saved through faith, and that not of yourselves; it is the gift of God, not of works, lest anyone should boast.' (NKJV)
+          </HoverCardContent>
+        </HoverCard>
+        <HoverCard>
+          <HoverCardTrigger>John 10:28, </HoverCardTrigger>
+          <HoverCardContent>
+            'And I give them eternal life, and they shall never perish; neither shall anyone snatch them out of My hand.' (NKJV)
+          </HoverCardContent>
+        </HoverCard>
+        <HoverCard>
+          <HoverCardTrigger>Romans 8:38-39)</HoverCardTrigger>
+          <HoverCardContent>
+            'For I am persuaded that neither death nor life, nor angels nor principalities nor powers, nor things present nor things to come, 39 nor height nor depth, nor any other created thing, shall be able to separate us from the love of God which is in Christ Jesus our Lord.' (NKJV)
+          </HoverCardContent>
+        </HoverCard>
+      </div>
+      <div className="my-4">That the Church is the body of Christ, which began on the day of Pentecost with Jesus Christ as the head. The Church is entrusted with the ministries of worship, edification, and evangelism. 
+        <HoverCard>
+          <HoverCardTrigger> (Acts 2:14-47, </HoverCardTrigger>
+          <HoverCardContent>
+            Due to the length of the passage, please read it on your own.
+          </HoverCardContent>
+        </HoverCard>
+        <HoverCard>
+          <HoverCardTrigger>Ephesians 2:20-23)</HoverCardTrigger>
+          <HoverCardContent>
+            'having been built on the foundation of the apostles and prophets, Jesus Christ Himself being the chief cornerstone, in whom the whole building, being fitted together, grows into a holy temple in the Lord, in whom you also are being built together for a dwelling place of God in the Spirit.' (NKJV)
+          </HoverCardContent>
+        </HoverCard>
+      </div>
+      <div className="my-4">That baptism and the Lord’s Supper are the only ordinances of the Church instituted by Jesus Christ. 
+        <HoverCard>
+          <HoverCardTrigger> (Matthew 28:19, </HoverCardTrigger>
+          <HoverCardContent>
+            'Go therefore and make disciples of all the nations, baptizing them in the name of the Father and of the Son and of the Holy Spirit,' (NKJV)
+          </HoverCardContent>
+        </HoverCard>
+        <HoverCard>
+          <HoverCardTrigger>1 Corinthians 11:23-26)</HoverCardTrigger>
+          <HoverCardContent>
+            'For I received from the Lord that which I also delivered to you: that the Lord Jesus on the same night in which He was betrayed took bread; and when He had given thanks, He broke it and said, “Take, eat; this is My body which is broken for you; do this in remembrance of Me.” In the same manner He also took the cup after supper, saying, “This cup is the new covenant in My blood. This do, as often as you drink it, in remembrance of Me.” For as often as you eat this bread and drink this cup, you proclaim the Lord’s death till He comes.' (NKJV)
+          </HoverCardContent>
+        </HoverCard>
+      </div>
+      <div className="my-4">That the Holy Spirit indwells every believer upon belief in the Lord Jesus.
+        <HoverCard>
+          <HoverCardTrigger> (Acts 2:1-4, </HoverCardTrigger>
+          <HoverCardContent>
+            'When the Day of Pentecost had fully come, they were all with one accord in one place. And suddenly there came a sound from heaven, as of a rushing mighty wind, and it filled the whole house where they were sitting. Then there appeared to them divided tongues, as of fire, and one sat upon each of them. And they were all filled with the Holy Spirit and began to speak with other tongues, as the Spirit gave them utterance.' (NKJV)
+          </HoverCardContent>
+        </HoverCard>
+        <HoverCard>
+          <HoverCardTrigger>Romans 8:9)</HoverCardTrigger>
+          <HoverCardContent>
+            'But you are not in the flesh but in the Spirit, if indeed the Spirit of God dwells in you. Now if anyone does not have the Spirit of Christ, he is not His.' (NKJV)
+          </HoverCardContent>
+        </HoverCard>
+      </div>
       <div className="my-4">That the Lord Jesus Christ has commissioned His Church to proclaim the Gospel to all people and make disciples of all nations. (Matthew 28:18-20)</div>
       <div>That the Church will be raptured, followed by the Great Tribulation and Christ’s return to the earth to establish His Millennial Kingdom. (Matthew 24:15-30, 1 Thessalonians 4:16-17, Revelation 20:4-6)</div>
     </div>
